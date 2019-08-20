5604434c79fd90df084503e8157acafa55de18ce
qinxiaoyi-e <qxy19950113>
2019-08-20 18:19:43 +0800

    first initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>hello!!</h1>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
